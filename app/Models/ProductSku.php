1d80616201deba7ef8f53bfac4df800a3411b508
Ihor Moskalyk <ihormosk@gmail.com>
2023-01-26 19:58:05 +0200

    added product sku attriutes table

--- app/Models/ProductSku.php
+++ app/Models/ProductSku.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class ProductSku extends Model
 {
@@ -13,5 +14,10 @@ class ProductSku extends Model
     public function product(): BelongsTo
     {
         return $this->belongsTo(Product::class);
+    }
+
+    public function attributes(): BelongsToMany
+    {
+        return $this->belongsToMany(Attribute::class)->withPivot('value');
     }
 }
---
+++ database/migrations/2023_01_26_175128_product_sku_attribute_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_sku_attribute', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_sku_id');
+            $table->unsignedBigInteger('attribute_id');
+            $table->integer('value');
+            $table->foreign('product_sku_id')->references('id')->on('product_skus')->onDelete('cascade');
+            $table->foreign('attribute_id')->references('id')->on('attributes')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_sku_attribute');
+    }
+};
